3c985d1e47c3c815a084fe250fea67e8e4bdcf59
joelprogramer <126760782+joelprogramer@users.noreply.github.com>
2023-11-07 11:30:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+"""
+namn="joel"
+nummer = 4
+
+nummer2 = 8
+
+nummer3 = nummer + nummer2
+
+nummer4 = 9
+
+print("hej! blabal")
+
+nummer=input()
+nummer2=input()
+
+
+print(int(nummer) * int(nummer2))
+
+print(int(nummer3) / int(nummer4))
+"""
+""""
+nummer = int(input())
+
+if nummer < 10:
+    print("nummer är mindre än tio")
+    
+if nummer > 10:
+    print("nummer är mer än tio")
+    
+
+if nummer == 10:
+    print("nummret är 10!")
+    
+    
+
+if nummer < 10:
+    print("nummer är minde än 10!")
+elif nummer > 10:
+    print("nummret är större än 10!")
+else:
+    print("nummret är 10!")    
+"""
+
+
+nummer1 = int(input())
+nummer2 = int(input())
+
+if nummer1 < 100:
+    print(nummer1 * nummer2)
+elif nummer1 > 100:
+    print(nummer1 / nummer2)
